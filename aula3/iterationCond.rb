2c269eb2d71699ba3dff0085b9f3b2a2f8db4531
Mateus Sobrinho <mateus.webphp@gmail.com>
2023-02-14 00:24:35 -0300

    dowhile and iteration with conditional

---
+++ aula3/iterationCond.rb
@@ -0,0 +1,27 @@
+result = ''
+loop do
+  puts result
+  puts 'Selecione uma das seguites opcoes'
+  puts '1- Descobrir a idade de uma pessoa'
+  puts '0- Sair'
+  
+  print 'Opcao: '
+
+  option = gets.chomp.to_i
+
+  if option == 1
+    print 'Digite o ano de nascimento: '
+    year_of_birth = gets.chomp.to_i
+    print 'Digite o ano atual: '
+    current_year = gets.chomp.to_i
+    age = current_year - year_of_birth
+    result = "Quem nasceu no ano de #{year_of_birth}, tem #{age} anos em #{current_year}"
+
+  elsif option == 0
+    break if option == 0
+  else
+    result  = 'Opcao invalida'
+  end
+
+  system "clear"
+end
